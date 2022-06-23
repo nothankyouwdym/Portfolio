81892e585f69413634deeeb237163dcdb2ec305b
nothankyouwdym <boubacar.diallo2028@uppublicschools.org>
2022-06-23 08:54:40 -0400

    1 file modified, 1 added

---
+++ src/Home.jsx
@@ -0,0 +1,17 @@
+const Home = () =>{
+  return(
+    <section>
+      <h1>Home</h1>
+
+      <p>
+        <strong>
+          <em>
+            Hey, welcome to my portfolio page my name is Boubacar and I just wanted to make one for fun and if I ever needed one.
+          </em>
+        </strong>
+      </p>
+    </section>
+  )
+}
+
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import {BrowserRouter,Routes,Route} from "react-router-dom"
 import './index.css'
 import About from './About'
 import Contact from "./Contact"
+import Home from './Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="/" element={<App />}>
             <Route path="About" element={<About />}></Route>
             <Route path="Contact" element={<Contact />}></Route>
+            <Route path="/" element={<Home />}></Route>
           </Route>
         </Routes>
   </BrowserRouter>
