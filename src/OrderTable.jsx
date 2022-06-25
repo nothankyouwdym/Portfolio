55ec6df10f4864b7f0c3a3f421532ed734c3a97f
nothankyouwdym <boubacar.diallo2028@uppublicschools.org>
2022-06-25 10:55:32 -0400

    1 file modified

--- src/OrderTable.jsx
+++ src/OrderTable.jsx
@@ -18,7 +18,7 @@ const TBody = (props) =>{
       return(
         <tr key={index}>
           <td>{row}</td>
-          <td><button onClick={Deselect(rows,index)}>Deselect</button></td>
+          <td><button onClick={Deselect}>Deselect</button></td>
         </tr>
       )
     })
